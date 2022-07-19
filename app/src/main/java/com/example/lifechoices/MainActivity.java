2c42ec4d6634a08d08a9b38f78392eb158785014
yankang <1003106735@qq.com>
2022-07-19 16:08:58 +0800

    我是 dev

--- app/src/main/java/com/example/lifechoices/MainActivity.java
+++ app/src/main/java/com/example/lifechoices/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我要学习
+        //我是dev
     }
 }
